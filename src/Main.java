460f89db1531d8f90abd34a8dd72269a85b8a791
leeseungwoo <vugil@naver.com>
2023-01-12 22:42:35 +0900

    Begin : 알고리즘 repository 생성

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String phone_number) {
+        int answer = 0;
+
+        return answer;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Solution test = new Solution();
+//        Scanner scan = new Scanner(System.in);
+//        int a = scan.nextInt();
+//        int s = scan.nextInt();
+        int[] i = new int[]{1, 2, 3, 4, 5};
+        String q = "010-2319-7772";
+        String w = "271";
+//        String[] p = new String[]{"ayaye", "uuu", "yeye", "yemawoo", "ayaayaa"};
+        System.out.println(test.solution(q));
+    }
+}
